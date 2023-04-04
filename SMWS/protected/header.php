8af84a714f6cf76dec6c579c95af893dc7ae9638
Ankush-gt <124658878+Ankush-gt@users.noreply.github.com>
2023-04-04 19:00:44 +0530

    Add files via upload

--- SMWS/protected/header.php
+++ SMWS/protected/header.php
@@ -1,12 +1,14 @@
 <?php
-// include("./include/constant.php");
-// include("./include/database.php");
-// include("./include/config.php");
-// include($_SERVER['DOCUMENT_ROOT'].'/smws/html/navbar.html');
+
 include($_SERVER['DOCUMENT_ROOT'].'/smws/include/constant.php');
 include($_SERVER['DOCUMENT_ROOT'].'/smws/include/database.php');
 include($_SERVER['DOCUMENT_ROOT'].'/smws/include/config.php');
+// include($_SERVER['DOCUMENT_ROOT'].'/projects/srs-b4-Intern-5/include/'.'include/constant.php');
+// include($_SERVER['DOCUMENT_ROOT'].'/projects/srs-b4-Intern-5/include/'.'include/database.php');
+// include($_SERVER['DOCUMENT_ROOT'].'/projects/srs-b4-Intern-5/include/'.'include/config.php');
 
-
+// include($_SERVER['DOCUMENT_ROOT'].'/projects/srs-b4-Intern-5/SMWS/include/constant.php ');
+// include(PROJECT_ROOT.'include/database.php');
+// include(PROJECT_ROOT.'include/config.php');
 
 ?>
